f1b1297942d6835c8cd5523de391235329f9aa91
cay <cay@zipcodes-MacBook-Pro-9.local>
2021-12-13 13:05:41 -0500

    added methods to split on ##

--- src/main/java/GroceryItemParse.java
+++ src/main/java/GroceryItemParse.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class GroceryItemParse {
+    public static boolean hasSymbol (String string) {
+        Pattern pattern = Pattern.compile("(:;)|(:%)|(:\\*)|(:@)|(:!)");
+        Matcher matcher = pattern.matcher(string);
+        return matcher.find();
+    }
+
+    private static List<String> splitStringIntoKVP(String stringPattern, String rawData) {
+        return new ArrayList<>(Arrays.asList(rawData.split(stringPattern)));
+    }
+
+    public static List<String> rawDataToArray(String rawData){
+        String stringPattern = "##";
+        return splitStringIntoKVP(stringPattern , rawData);
+    }
+
+
 }
