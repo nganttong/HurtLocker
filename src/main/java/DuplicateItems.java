0106732acf1f5fac1f39eb3d55aaad24e5b8920b
cay <cay@zipcodes-MacBook-Pro-9.local>
2021-12-13 10:53:58 -0500

    set up duplicate items

--- src/main/java/DuplicateItems.java
+++ src/main/java/DuplicateItems.java
@@ -2,4 +2,34 @@ public class DuplicateItems {
     private String name;
     private int count;
     private Prices prices;
+
+    public DuplicateItems(String name, int count, Prices prices) {
+        this.name = name;
+        this.count = count;
+        this.prices = prices;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public Prices getPrices() {
+        return prices;
+    }
+
+    public void setPrices(Prices prices) {
+        this.prices = prices;
+    }
 }
