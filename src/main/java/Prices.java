3629f3f61b9b40ce03c4c73814a50c3d0ab4c967
cay <cay@zipcodes-MacBook-Pro-9.local>
2021-12-13 10:40:37 -0500

    messing with prices class

--- src/main/java/Prices.java
+++ src/main/java/Prices.java
@@ -4,7 +4,16 @@ public class Prices {
     private HashMap<Double, Integer> prices = new HashMap<Double, Integer>();
 
     //see how many times the same price appears
+    public void howManyTimesPriceAppears (double price){
+        //(price, counting occurrences)
+        prices.put(price, prices.get(price) + 1);
+    }
+
     //add new prices
+    public void addPrice (double price) {
+        prices.put(price, 1);
+    }
+
     //some dont have prices????
     //see all prices
     //see how many have prices?
